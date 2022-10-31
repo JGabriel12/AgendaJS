e04cc47517a9afa75fdcb33ec6be7ac7299efb1f
Joao Gabriel Marinho <jgabriel.marinho.dev@hotmail.com>
2022-10-31 18:08:38 -0300

    lib validator and user registration working

--- src/controllers/cadastroController.js
+++ src/controllers/cadastroController.js
@@ -1,3 +1,28 @@
+const Cadastro = require('../models/CadastroModel')
+
 exports.index = (req, res) => {
   res.render('cadastro')
+}
+
+exports.register = async (req, res) => {
+  const cadastro = new Cadastro(req.body)
+  await cadastro.register()
+
+  try {
+    if (cadastro.errors.length > 0) {
+      req.flash('errors', cadastro.errors)
+      req.session.save(function () {
+        return res.redirect('/cadastro/index')
+      })
+      return
+    }
+
+    req.flash('success', 'Cadastrado com sucesso')
+    req.session.save(function () {
+      return res.redirect('/login/index')
+    })
+  } catch (e) {
+    console.log(e)
+    return res.render('404')
+  }
 }
